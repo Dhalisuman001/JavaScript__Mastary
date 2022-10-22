affb7c6988fffa92dda6ce4707afe03650362501
Suman Dhali <dhalisuman001@gmail.com>
2022-10-22 09:32:38 +0530

    Dom manipulation: event handeling

--- 02_JavaScript/GuessMyNumber/script.js
+++ 02_JavaScript/GuessMyNumber/script.js
@@ -1,11 +1,22 @@
 'use strict';
 
-document.querySelector('.message').textContent = "Correct Number 🥳";
-console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = "Correct Number 🥳";
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 100;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 100;
 
-document.querySelector('.guess').value = 34;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 34;
+// console.log(document.querySelector('.guess').value);
+
+
+document.querySelector('.check').addEventListener('click',()=>{
+   const guess = Number(document.querySelector('.guess').value);
+   console.log(guess, typeof guess);
+
+   if (!guess) {
+    document.querySelector('.message').textContent = "⛔ No number!";
+   }
+
+})
 
