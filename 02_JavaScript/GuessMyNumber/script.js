29ef84ecb0bfbc82b788b1f37cb3b27c42fda05f
Suman Dhali <dhalisuman001@gmail.com>
2022-10-22 09:16:07 +0530

    Dom manipulation: querySelector, textContent, value

--- 02_JavaScript/GuessMyNumber/script.js
+++ 02_JavaScript/GuessMyNumber/script.js
@@ -1,3 +1,11 @@
 'use strict';
 
+document.querySelector('.message').textContent = "Correct Number 🥳";
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 100;
+
+document.querySelector('.guess').value = 34;
+console.log(document.querySelector('.guess').value);
+
