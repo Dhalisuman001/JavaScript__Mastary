784ccf157c16ed7fdb2b46f29ae60485585f31a9
Suman Dhali <dhalisuman001@gmail.com>
2022-10-28 20:45:37 +0530

    Module-4 : Array Destructuring

--- 04_ModernOperator/FoodDelivery/script.js
+++ 04_ModernOperator/FoodDelivery/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (startIndex, mianIndex) {
+    return [this.starterMenu[startIndex], this.mainMenu[mianIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,42 @@ const restaurant = {
     },
   },
 };
+
+// destructring arrays
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+let [first, sec] = restaurant.categories;
+console.log(first, sec);
+
+// swapping manully
+// const temp = first;
+// first = sec;
+// sec = temp;
+// console.log(first, sec);
+
+// swapping modern way
+[first, sec] = [sec, first];
+console.log(first, sec);
+
+// destructure retutn value
+const [starter, main] = restaurant.order(2, 0);
+console.log(starter, main);
+
+// destructure in nested array
+const nested = [2, 8, [6, 1]];
+
+// const [i, , j] = nested;
+// console.log(i, j);
+
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// default values
+const [n1 = 7, n2 = 4, n3 = 1] = [8, 0];
+console.log(n1, n2, n3);
