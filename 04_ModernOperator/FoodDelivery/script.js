329e56be3d95e919c81a4399c29fe4350b38a131
Suman Dhali <dhalisuman001@gmail.com>
2022-11-02 22:44:01 +0530

    Module 4: Coding challeng 1

--- 04_ModernOperator/FoodDelivery/script.js
+++ 04_ModernOperator/FoodDelivery/script.js
@@ -54,6 +54,107 @@ const restaurant = {
     console.log(otherIng);
   },
 };
+
+//coding challange -->1
+// 1. Create one player array for each team (variables 'players1' and
+// 'players2')
+// 2. The first player in any player array is the goalkeeper and the others are field
+// players. For Bayern Munich (team 1) create one variable ('gk') with the
+// goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
+// field players
+// 3. Create an array 'allPlayers' containing all players of both teams (22
+// players)
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+// new array ('players1Final') containing all the original team1 players plus
+// 'Thiago', 'Coutinho' and 'Perisic'
+// 5. Based on the game.odds object, create one variable for each odd (called
+// 'team1', 'draw' and 'team2')
+// 6. Write a function ('printGoals') that receives an arbitrary number of player
+// names (not an array) and prints each of them to the console, along with the
+// number of goals that were scored in total (number of player names passed in)
+// 7. The team with the lower odd is more likely to win. Print to the console which
+// team is more likely to win, without using an if/else statement or the ternary
+// operator.
+// Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
+// Then, call the function again with players from game.scored
+// GOOD LUCK
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//1.
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+//2.
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+//3.
+const allPlayer = [...players1, ...players2];
+console.log(allPlayer);
+
+//4.
+const playersFinal = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(playersFinal);
+
+//5.
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1, team2, draw);
+
+//6.
+const printGoals = (...names) => {
+  console.log('Goal scorer list: ');
+  names.forEach(e => {
+    console.log(e);
+  });
+  console.log('Total goal scored: ', names.length);
+};
+printGoals(...game.scored);
+
+//7.
+team1 > team2 && console.log('Team1 is more likily to win');
+team1 < team2 && console.log('Team2 is more likily to win');
+
+/*
 // nullish coalsing operator
 
 restaurant.numGuests = 0;
@@ -63,6 +164,7 @@ console.log(guest1);
 //using ternary
 const guest2 = restaurant.numGuests ?? 10;
 console.log(guest2);
+*/
 
 /*
 // Short cuircuiting(&&,||)
