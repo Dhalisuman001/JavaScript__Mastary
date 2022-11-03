f8758e7446b35626a684eb6f4580e567fac55d2d
Suman Dhali <dhalisuman001@gmail.com>
2022-11-03 09:22:35 +0530

    Module 4: Coding challange 2

--- 04_ModernOperator/FoodDelivery/script.js
+++ 04_ModernOperator/FoodDelivery/script.js
@@ -56,6 +56,98 @@ const restaurant = {
     console.log(otherIng);
   },
 };
+//Coding challange -->2
+// 1. Loop over the game.scored array and print each player name to the console,
+// along with the goal number (Example: "Goal 1: Lewandowski")
+// 2. Use a loop to calculate the average odd and log it to the console (We already
+// studied how to calculate averages, you can go check if you don't remember)
+// 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+// Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them
+// (except for "draw"). Hint: Note how the odds and the game objects have the
+// same property names 😉
+// 4. Bonus: Create an object called 'scorers' which contains the names of the
+// players who scored as properties, and the number of goals as the value. In this
+// game, it will look like this:
+// {
+// Gnarby: 1,
+// Hummels: 1,
+// Lewandowski: 2
+// }
+// GOOD LUCK 😀
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//1.
+for (const [key, value] of game.scored.entries()) {
+  console.log(`Goal no. ${key + 1} scored by ${value}`);
+}
+
+//2.
+let sum = 0;
+let count = 0;
+for (var [key, value] of Object.entries(game.odds)) {
+  sum += value;
+}
+const average = sum / key.length;
+console.log(average);
+
+//3.
+for (const [key, value] of Object.entries(game.odds)) {
+  const resultStr = `Odd of ${key === 'x' ? '' : 'victory'} ${
+    key === 'x' ? 'draw' : game[key]
+  }: ${value}`;
+  console.log(resultStr);
+}
+
+//4.
+const scores = {};
+for (const value of game.scored) {
+  scores[value] ? scores[value]++ : (scores[value] = 1);
+}
+console.log(scores);
+
+/*
 // loop over object
 //object keys
 const attr = Object.keys(openingHours);
@@ -77,6 +169,8 @@ console.log(entry);
 entry.forEach(([key, { open, close }]) => {
   console.log(`On ${key}: we open at ${open} and close at ${close}`);
 });
+
+*/
 
 /*
 // optional chaning
