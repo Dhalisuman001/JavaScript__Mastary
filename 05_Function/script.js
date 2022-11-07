3d37d0e4f20f88c3f28bf61e98debd2dbd8d1f14
Suman Dhali <dhalisuman001@gmail.com>
2022-11-07 17:58:40 +0530

    Module 5: passing value & reference

--- 05_Function/script.js
+++ 05_Function/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
+/*
 //Default parameter
-
 const booking__history = [];
 
 const flightBooking = (flightId, passCount = 1, price = 5600 * passCount) => {
@@ -20,3 +20,26 @@ const flightBooking = (flightId, passCount = 1, price = 5600 * passCount) => {
 
 flightBooking('LH534');
 flightBooking('LP764', 2, 10000);
+*/
+
+// passing value and reference
+const flightId = 'LH534';
+const MyData = {
+  name: 'Suman Dhali',
+  id: 622153475847,
+};
+
+const checkIn = (flightID, passenger) => {
+  flightID = 'LP296';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.id === 622153475847) {
+    alert('Booked');
+  } else {
+    alert('Worng details');
+  }
+};
+
+checkIn(flightId, MyData);
+console.log(flightId);
+console.log(MyData);
