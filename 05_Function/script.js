ed3ee49098b7c9576d27f3ee0e50bec21598d6eb
Suman Dhali <dhalisuman001@gmail.com>
2022-11-07 17:38:48 +0530

    Module 5: default parameter

--- 05_Function/script.js
+++ 05_Function/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+//Default parameter
+
+const booking__history = [];
+
+const flightBooking = (flightId, passCount = 1, price = 5600 * passCount) => {
+  // ES5
+  //   passCount = passCount || 1;
+  //   price = price || 5600;
+
+  const booking = {
+    flightId,
+    passCount,
+    price,
+  };
+  booking__history.push(booking);
+  console.log(booking);
+};
+
+flightBooking('LH534');
+flightBooking('LP764', 2, 10000);
